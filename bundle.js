86ef7df7b34284e4d1338b7262e9dded611f358b
Mukeshcbvxv <bgfffcgkjch@gmail.com>
2025-06-21 09:58:00 +0530

    Add files via upload

---
+++ bundle.js
@@ -0,0 +1,5 @@
+
+document.getElementById("root").innerHTML = `
+  <h1 style='text-align:center;color:#4F46E5;'>Goswami Tech Blog</h1>
+  <p style='text-align:center;'>यहाँ आपको नवीनतम टेक्नोलॉजी की जानकारी मिलेगी।</p>
+`;
